f633ef41c88666959252ae9888eb7238bcc7f282
rouvoy <romain.rouvoy@gmail.com>
2014-04-03 12:11:17 +0200

    Adding headers

--- src/test/java/fr/inria/minibus/SubscribeTest.java
+++ src/test/java/fr/inria/minibus/SubscribeTest.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright (c) 2014 Inria, University Lille 1.
+ *
+ * This file is part of Minibus.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ *
+ * Contact: romain.rouvoy@univ-lille1.fr.
+ */
 package fr.inria.minibus;
 
 import org.junit.Test;
